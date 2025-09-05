e0f79a985bc1eb699c68adbe013c7ef1130da9c5
lcgray77 <lcgray1977@gmail.com>
2025-09-05 17:30:54 -0400

    Update tv-intro.js

--- tv-intro.js
+++ tv-intro.js
@@ -1,1 +1,12 @@
+// Wait a few seconds, then fade out the TV intro
+setTimeout(() => {
+  const intro = document.getElementById('tv-intro');
+  intro.classList.add('fade-out');
+}, 4000); // TV intro lasts 4 seconds
+
+// Optional: remove overlay from DOM after fade
+document.getElementById('tv-intro').addEventListener('animationend', () => {
+  document.getElementById('tv-intro').remove();
+});
+
 
